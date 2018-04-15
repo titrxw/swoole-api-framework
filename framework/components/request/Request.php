2ab8525ad7f5fe9fe4ee8264c7f8cdb1bccf9519
titrxw <1120309488@qq.com>
2018-04-15 17:01:18 +0800

    添加获取客户端ip方法

--- framework/components/request/Request.php
+++ framework/components/request/Request.php
@@ -126,6 +126,67 @@ class Request extends Component
         return $result;
     }
 
+    public function getClientIp()
+    {
+        $realip = NULL;
+
+        if (isset($_SERVER))
+        {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            {
+                $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+                /* 取X-Forwarded-For中第一个非unknown的有效IP字符串 */
+                foreach ($arr AS $ip)
+                {
+                    $ip = trim($ip);
+
+                    if ($ip != 'unknown')
+                    {
+                        $realip = $ip;
+
+                        break;
+                    }
+                }
+            }
+            elseif (isset($_SERVER['HTTP_CLIENT_IP']))
+            {
+                $realip = $_SERVER['HTTP_CLIENT_IP'];
+            }
+            else
+            {
+                if (isset($_SERVER['REMOTE_ADDR']))
+                {
+                    $realip = $_SERVER['REMOTE_ADDR'];
+                }
+                else
+                {
+                    $realip = '0.0.0.0';
+                }
+            }
+        }
+        else
+        {
+            if (getenv('HTTP_X_FORWARDED_FOR'))
+            {
+                $realip = getenv('HTTP_X_FORWARDED_FOR');
+            }
+            elseif (getenv('HTTP_CLIENT_IP'))
+            {
+                $realip = getenv('HTTP_CLIENT_IP');
+            }
+            else
+            {
+                $realip = getenv('REMOTE_ADDR');
+            }
+        }
+
+        preg_match("/[\d\.]{7,15}/", $realip, $onlineip);
+        $realip = !empty($onlineip[0]) ? $onlineip[0] : '0.0.0.0';
+
+        return $realip;
+    }
+
     public function __destruct()
     {
         unset($_POST,$_GET,$_REQUEST);
