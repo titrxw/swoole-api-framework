9551d6f801ca98a3bb8348fef6232ed67bc3058e
titrxw <1120309488@qq.com>
2018-05-26 14:04:04 +0800

    整理

--- framework/client/TcpClient.php
+++ framework/client/TcpClient.php
@@ -7,7 +7,9 @@ class TcpClient extends BaseClient
   protected $_client;
   protected function init()
   {
-    $this->_client = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+    if (!$this->_client) {
+      $this->_client = new \swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+    }
     parent::init();
   }
 }
