3878ba01a8468bf24eb6c84d82269aa2351197cc
rxw <1120309488@qq.com>
2018-08-20 21:59:12 +0800

    Update RpcClient.php

--- framework/client/RpcClient.php
+++ framework/client/RpcClient.php
@@ -3,6 +3,7 @@ namespace framework\client;
 
 use framework\base\Component;
 use Thrift\ClassLoader\ThriftClassLoader;
+use Thrift\Protocol\TMultiplexedProtocol;
 use Thrift\Protocol\TBinaryProtocol;
 use Thrift\Transport\TSocket;
 use Thrift\Transport\TFramedTransport;
@@ -16,14 +17,17 @@ $loader->register(TRUE);
 
 class RpcClient extends Component
 {
-  public function call($host, $port, $class, $method, ...$args)
+  public function call($host, $port, $service, $method, ...$args)
   {
     $socket = new TSocket($host, $port);
     $transport = new TFramedTransport($socket);
     $protocol = new TBinaryProtocol($transport);
+    $Service = new TMultiplexedProtocol($protocol, $service);
     $transport->open();
-    
-    $client = new $class($protocol);
+
+    $namespace = \substr($service, 0 , \strpos($service, 'Service'));
+    $class = '\\services\\' . $namespace . '\\' . $service . 'Client';
+    $client = new $class($Service);
     $ret = $client->$method(...$args);
     $transport->close();
 
