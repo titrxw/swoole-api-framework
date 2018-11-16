1bd953180c2b31d595ed6c69cd2e629627d0bbec
rxw <1120309488@qq.com>
2018-11-16 16:37:09 +0800

    Update ZookeeperRpcServer.php

--- framework/server/ZookeeperRpcServer.php
+++ framework/server/ZookeeperRpcServer.php
@@ -7,6 +7,9 @@
  */
 namespace framework\server;
 
+/**
+ * 需要利用zookeeper进行节点的管理，当节点退出的时候，把对应的service里包含的server踢出
+ */
 class ZookeeperRpcServer extends RpcServer
 {
   protected $_process;
@@ -32,15 +35,38 @@ class ZookeeperRpcServer extends RpcServer
     return true;
   }
 
+  public function serveChange($event_type, $stat, $path)
+  {
+    $workers = $this->_zookeeper->getHandle()->getChildren($path);     
+    $size = count($workers);
+    var_dump($workers);
+    
+    $servers = [];
+		for ($i = 0; $i < $size; $i ++) {
+          $res = $this->_zookeeper->getHandle()->get($path . '/' . $workers[$i - 1]);
+          $res = json_decode($res, true);
+          $servers[] = $res;
+    }
+    var_dump($servers);
+    $this->_zookeeper->getHandle()->set($path, json_encode($servers));
+  }
+
+  /**
+   * 目前没有处理一个service多个server的问题
+   */
   protected function registerServices()
   {
     $services = $this->getValueFromConf('services', []);
     foreach ($services as $key => $value) {
-      $this->_zookeeper->getHandle()->set($key, json_encode([
+      if (!$this->_zookeeper->getHandle()->exists($key)) {
+        $this->_zookeeper->getHandle()->makeNode($key, null);
+      }
+      // 监控该节点的
+      $this->_zookeeper->getHandle()->watch($key, [$this, 'serveChange']);
+		  $this->znode = $this->_zookeeper->getHandle()->makeNode($key . '/w-', json_encode([
         'host' => $this->_conf['ip'],
-        'port' => $this->_conf['port'],
-        'status' => 200
-      ]));
+        'port' => $this->_conf['port']
+      ]), null, \Zookeeper::EPHEMERAL | \Zookeeper::SEQUENCE);
     }
   }
 }
