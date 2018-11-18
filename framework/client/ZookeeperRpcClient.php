c586c453002a4fa4de35c3d6e9fcb0c0c917eedb
rxw <1120309488@qq.com>
2018-11-18 13:27:10 +0800

    Update ZookeeperRpcClient.php

--- framework/client/ZookeeperRpcClient.php
+++ framework/client/ZookeeperRpcClient.php
@@ -10,16 +10,25 @@ class ZookeeperRpcClient extends RpcClient
     $this->_zookeeper = $this->getComponent(SYSTEM_APP_NAME, $this->getValueFromConf('conf', 'zookeeper'));
   }
 
+  public function getServer($path)
+  {
+    $workers = $this->_zookeeper->getHandle()->getChildren($path);     
+    $worker = mt_rand(0, \count($workers) - 1);
+    $res = $this->_zookeeper->getHandle()->get($path . '/' . $worker);
+    $res = json_decode($res, true);
+    if (\is_array($res)) {
+      return $res;
+    }
+    return false;
+  }
+
   public function call($service, $method, ...$args)
   {
     // 根据service从zookeeper上获取对应的service的服务器地址
-    $info = $this->_zookeeper->getHandle()->get($service);
+    $info = $this->getServer($service);
     if (!$info) {
       return [404];
     }
-    $info = json_decode($info, true);
-    // 这里是数组
-    $info = $info[mt_rand(0, \count($info) - 1)];
 
     return parent::call($info['host'], $info['port'], $service, $method, ...$args);
     
